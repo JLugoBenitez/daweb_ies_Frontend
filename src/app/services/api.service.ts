c6a662c208a81804dce9f98267bfeff2caff64c6
Javier <javierlugobenitez7@gmail.com>
2026-05-13 08:58:37 +0200

    configuracion render

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { Alumno, Profesor, Asignatura, Matricula } from '../models/ies.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://daweb-ies-mren.onrender.com/' // Reemplaza esto con la URL de tu backend en Render
+};
