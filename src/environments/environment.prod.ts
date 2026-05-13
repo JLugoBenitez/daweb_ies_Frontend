3f1f57fa65352e520d3a5d88ffd0be8f2709f7e0
Javier <javierlugobenitez7@gmail.com>
2026-05-13 09:01:38 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://daweb-ies-mren.onrender.com/' // Reemplaza esto con la URL de tu backend en Render
+  apiUrl: 'https://daweb-ies-mren.onrender.com' // Reemplaza esto con la URL de tu backend en Render
 };
